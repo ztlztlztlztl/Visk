ee2fcbc11a260b272f41f926d9e9f08780fbb085
ztlztlztlztl <ztl860598@gmail.com>
2026-06-01 13:30:27 +0800

    add ignore

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Visk_FILE D:/Visk/Visk/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/Visk.exe)
-set(__QT_DEPLOY_TARGET_Visk_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Visk_RUNTIME_DLLS D:/Qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.11.0/mingw_64/bin/Qt6Concurrent.dll;D:/Qt/6.11.0/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.11.0/mingw_64/bin/Qt6Core.dll)
--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/deploy_Visk_17b78b518e.cmake
+++
@@ -1,8 +0,0 @@
-include("D:/Visk/Visk/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/Visk-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "D:/Visk/Visk/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/Visk.exe"
-    GENERATE_QT_CONF
-)
